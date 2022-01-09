ea5168b193137a872edd04ef9ad78146ab728143
erhic <eric.gichovi@student.moringaschool.com>
2022-01-09 12:24:09 +0300

    imported a child component

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quote } from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quote:Quote[] = [
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
+  ];
 
   constructor() { }
 
