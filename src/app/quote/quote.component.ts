3b6093c64137f84a1ae08b71f5ae745b1d6d7bc9
erhic <eric.gichovi@student.moringaschool.com>
2022-01-09 18:28:09 +0300

    set two data binding in form

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,12 +8,12 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[] = [
-    {person:'Alex', quoteWords:`"never never finding Nemo"`,author:'John',upvote:20,downvote:2,timeElapsed:new Date()},
-    {person:'Miriam',quoteWords:`"Buy Cookies"`,author:'Brian',upvote:20,downvote:2,timeElapsed:new Date()},
-    {person:'Ann',quoteWords:`"Get new Phone Case"`,author:'Eistien',upvote:20,downvote:2,timeElapsed:new Date()},
-    {person:'Joan',quoteWords:`"Get Dog Food"`,author:'Bob',upvote:20,downvote:2,timeElapsed:new Date()},
-    {person:'Tedd',quoteWords:`"Solve math homework"`,author:'Nelson Mandela',upvote:20,downvote:2,timeElapsed:new Date()},
-    {person:'Mark',quoteWords:`"Plot my world domination plan"`,author:'Lurther King',upvote:20,downvote:2,timeElapsed:new Date()}
+    {person:'Alex', quoteWords:`"never never finding Nemo"`,author:'John',upvote:20,downvote:2,timeElapsed:new Date(2021-10-21)},
+    {person:'Miriam',quoteWords:`"Buy Cookies"`,author:'Brian',upvote:12,downvote:1,timeElapsed:new Date()},
+    {person:'Ann',quoteWords:`"Get new Phone Case"`,author:'Eistien',upvote:42,downvote:16,timeElapsed:new Date()},
+    // {person:'Joan',quoteWords:`"Get Dog Food"`,author:'Bob',upvote:20,downvote:2,timeElapsed:new Date()},
+    // {person:'Tedd',quoteWords:`"Solve math homework"`,author:'Nelson Mandela',upvote:20,downvote:2,timeElapsed:new Date()},
+    // {person:'Mark',quoteWords:`"Plot my world domination plan"`,author:'Lurther King',upvote:20,downvote:2,timeElapsed:new Date()}
   ];
 
   
